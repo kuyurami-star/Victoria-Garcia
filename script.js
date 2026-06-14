6c7ab7eb064989d366eaea9d1bbb3754bd635cc5
kuyurami-star <kuyurami@gmail.com>
2026-06-14 14:30:57 -0400

    Victoria Garcia

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const ElNombre = document.getElementById('nombre')
+const Elintro = document.getElementById('descripcion')
+const LaMeta = document.getElementById('lista de fortalezas')
+const Listaf = document.getElementById('listaf de oportunidades')
+const ListaO = document.getElementById('listaO de debilidades')
+const ListaD = document.getElementById('listaD de amenazas')
+
+const elUnicontainer= document.getElementById('Unicontainer')
+const boton = document.getElementById('btnActualizar')
+
+function cargaPerfil()
+         //Datos de Ejemplo
+    const Datos = (
+        nombre:"Victoria Garcia"
+        descripcion:"Graduarme de 5 to Año y estudiar la Carrera que me apasiona y ser exitosa en la vida"
+        fortalezas:" Responsable y Apasionada"
+        oportunidades:"Tengo conocimientos de los temas"
+        debilidades:"Perfecionista e Impaciente"
+        amenazas:"alta competencia laboral"
